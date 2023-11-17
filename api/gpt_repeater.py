4fe7ebcb9a450eca557279109e73b8304ac4d470
Seliverstov <seli@verstov.pro>
2023-11-17 22:58:17 +0400

    add conf/logger.dist

--- api/gpt_repeater.py
+++ api/gpt_repeater.py
@@ -15,22 +15,23 @@ logger = logging.getLogger(__name__)
 
 class GptSeoRequest(BaseModel):
     title: str
+    description: str
     keys: list[str]
 
 
 @app.post('/api/gpt_repeater')
 async def gpt_repeater(request: Request, data: GptSeoRequest):
+    config: Config = request.state.config
+    if request.client.host not in config.gpt.allowed_ips:
+        raise HTTPException(status_code=403, detail="Access denied")
     if data.title == '/test':
         await asyncio.sleep(5)
         return {'result': lorem.text()}
-    config: Config = request.state.config
-    openai_client = AsyncOpenAI(api_key=config.gpt.gpt_token)
-    if request.client.host not in config.gpt.allowed_ips:
-        raise HTTPException(status_code=403, detail="Access denied")
 
+    openai_client = AsyncOpenAI(api_key=config.gpt.gpt_token)
     keys = list(filter(lambda x: len(x) > 2, data.keys))
     keys = "\n".join(keys)
-    bot_msg = config.gpt.gpt_message.format(title=data.title, keys=keys)
+    bot_msg = config.gpt.gpt_message.format(title=data.title, description=data.description,keys=keys)
     messages = [
         {"role": "user", "content": bot_msg},
     ]
